e3bc01d29488ff982113170c53fbc17be189c8a9
Alexandre Previ <alexandre.previ@gmail.com>
2021-04-07 16:33:32 +0200

    Revert "Feature/user service"

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import { Router, Request, Response, NextFunction, request, response } from 'express'
 import { RouterDeps } from '../types/app.types'
 import { sendOk200Response, sendNotOk200Response, sendNotOk503Response, sendNotOk404Response } from './responses'
-import { dtoValidationMiddleware } from '../middlewares/dtoValidation.middleware'
-import { createUserSchema } from '../utils/dtoValidationSchemas'
 
 export const RouteHandler = (deps: RouterDeps): Router => {
     const { logger, controllers } = deps
@@ -53,20 +51,6 @@ export const RouteHandler = (deps: RouterDeps): Router => {
         try {
             const { id } = req.params
             const { success, data } = await controllers.userController.getUserById(id)
-            if (success) {
-                sendOk200Response(req, res, data)
-            } else {
-                sendNotOk503Response(req, res, data)
-            }
-        } catch (error) {
-            next(error)
-        }
-    })
-
-    router.post('/createuser', dtoValidationMiddleware(createUserSchema), async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const user = req.body
-            const { success, data } = await controllers.userController.createUser(user)
             if (success) {
                 sendOk200Response(req, res, data)
             } else {

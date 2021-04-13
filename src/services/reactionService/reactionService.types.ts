3bca6d832baa08d75c884dbecbed9cd526880e20
Alexandre Previ <alexandre.previ@gmail.com>
2021-04-13 16:01:52 +0200

    created reactionController and Service

---
+++ src/services/reactionService/reactionService.types.ts
@@ -0,0 +1,14 @@
+export type ReactionResponse = {
+    success: boolean
+    data: string
+}
+
+export type ReactionPayload = {
+    name: string
+    action: string
+}
+
+export interface ServiceError {
+    success: boolean
+    data: string
+}
